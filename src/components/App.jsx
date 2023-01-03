ad50f0fb3b9e9349b5934b608d1a80749f5e3275
Ruslan Tendora <ruslantendora@gmail.com>
2023-01-03 12:32:34 +0200

    add start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+
+import React, { Component } from 'react';
+import { Form } from './Form/Form';
+
+import { ContactsList } from './ContactsList/ContactsList'
+import { Filter } from './Filter/Filter';
+import css from './App.module.css'
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: ''
+  }
+
+  formSubmitHandler = (date) => {
+    
+    this.setState(prevState => {
+      const names = []
+
+      prevState.contacts.map(contact => {
+       return names.push(contact.name)
+      })
+
+      if (names.includes(date.name)) {
+        return alert(`${date.name} is already in contacts`)
+      }
+      
+      return { contacts: [date, ...prevState.contacts]}
+    })  
+  }
+
+
+  deletContact = (id) => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id)
+    }))
+  }
+  
+
+  filterChange = (e) => {
+    this.setState({ filter: e.currentTarget.value })
+  }
+
+  render() {
+    const { filter, contacts } = this.state
+
+    const normalizeFilter = filter.toLowerCase()
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter))
+
+    return (
+        <div className={css.container}>
+          <h1>Phonebook</h1>
+
+          <Form onSubmit={this.formSubmitHandler} />
+
+          <h2>Contacts:</h2>
+          <Filter
+            value={filter}
+            onChange={this.filterChange} />
+        
+        <ContactsList contacts={filteredContacts} onDeletContact={this.deletContact} />
+
+        </div>
+    );
+  }
+  
 };
